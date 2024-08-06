4e67d3980db8da83f2e3773de747905a5bed4d1e
SahilSinha3 <sahil@pgagi.in>
2024-08-06 11:47:26 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { FaCopy } from 'react-icons/fa';
 
 function App() {
+  const copyPhoneNumber = () => {
+    navigator.clipboard.writeText('+12099894454');
+    alert('Phone number copied!');
+  };
+
+  const handleCall = () => {
+    window.location.href = 'tel:+12099894454';
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="/TOINGG.png" alt="Company Logo" className="logo" />
       </header>
+      <main className="main-content">
+        <h1 className="main-heading animate-text">Still hiring humans</h1>
+        <p className="sub-heading">Why not AI?</p>
+        <div className="contact-box">
+          <a href="tel:+12099894454" className="phone-number" onClick={handleCall}>+1 209 989 4454</a>
+          <button onClick={copyPhoneNumber} className="copy-button" aria-label="Copy Phone Number">
+            <FaCopy />
+          </button>
+        </div>
+      </main>
     </div>
   );
 }
